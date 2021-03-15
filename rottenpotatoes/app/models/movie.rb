875299b7e266190c6c53de7e9d4a247d3cd1b6f4
elizabethhokaj <78813068+elizabethhokaj@users.noreply.github.com>
2021-03-14 20:27:16 -0500

    Update movie.rb

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+  
+  def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+  end
+  
+  def change
+    add_column :movies, :director, :string
+  end
+  
+  def self.similar(id)
+    director = Movie.find(id).director
+    self.where(director: director)
+  end
+  
+  
 end
